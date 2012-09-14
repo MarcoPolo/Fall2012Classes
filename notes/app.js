d9c73073eb9b04ab796d65d64732583a7ad76b75
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-09-14 09:08:14 -0400

    moved template engine to be markdown, finished hw2 for numerical analysis

--- notes/app.js
+++ notes/app.js
@@ -6,20 +6,47 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , fs = require('fs')
+  , marked = require('marked');
+
+
+// Set default options
+marked.setOptions({
+  gfm: true,
+  pedantic: false,
+  sanitize: false,
+  // callback for code highlighter
+  highlight: function(code, lang) {
+    if (lang === 'js') {
+      return javascriptHighlighter(code);
+    }
+    return code;
+  }
+});
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  //app.set('view engine', 'jade');
+  //app.set('view engine', 'markdown');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+
+  app.engine('md', function(path, options, fn){
+    fs.readFile(path, 'utf8', function(err, str){
+      if (err) return fn(err);
+      str = marked(str);
+      fn(null, str);
+    });
+  });
+
 });
 
 app.configure('development', function(){
@@ -28,8 +55,14 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/class',routes.class);
+
 app.get('/numAnalysis',routes.numAnalysis);
+app.get('/numAnalysis/:id', routes.numAnalysisPage)
+
 app.get('/modern',routes.modern);
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- notes/routes/index.js
+++ notes/routes/index.js
@@ -3,12 +3,20 @@
  * GET home page.
  */
 
+var markdown = require('markdown').markdown
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 exports.numAnalysis = function(req, res){
   res.render('numAnalysis', { title: 'Numerical Analysis' });
 };
+
+exports.numAnalysisPage = function(req, res){
+    res.render('numAnalysis/'+req.params.id+'.md', {title: req.params.id})
+};
+
+
 exports.modern = function(req, res){
   res.render('modern', { title: 'Modern Physics' });
 };
